91f176d6950b41930432604b8af6f1f98e640865
ndaru <ndaru1990@gmail.com>
2023-01-20 15:43:05 +0700

    formatting aja

--- routes/noteRoutes.js
+++ routes/noteRoutes.js
@@ -1,11 +1,12 @@
-const express = require('express')
-const router = express.Router()
-const notesController = require('../controllers/notesController')
+const express = require("express");
+const router = express.Router();
+const notesController = require("../controllers/notesController");
 
-router.route('/')
-    .get(notesController.getAllNotes)
-    .post(notesController.createNewNote)
-    .patch(notesController.updateNote)
-    .delete(notesController.deleteNote)
+router
+  .route("/")
+  .get(notesController.getAllNotes)
+  .post(notesController.createNewNote)
+  .patch(notesController.updateNote)
+  .delete(notesController.deleteNote);
 
 module.exports = router
